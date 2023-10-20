bd9f6e5eabfdf1695eede64b0c4ec1f9ed7b7646
Jesse Reynolds <jessereynolds18@gmail.com>
2023-10-19 21:28:34 -0500

    create a payroll system using 'this'

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeData) {
+    return {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    const date = dateStamp;
+
+    const timeOutEvent = this.timeOutEvents.find(timePunch => timePunch.date === dateStamp);
+    const timeInEvent = this.timeInEvents.find(timePunch => timePunch.date === dateStamp);
+
+    if (timeInEvent && timeOutEvent) {
+        const timeOutPunch = timeOutEvent.hour;
+        const timeInPunch = timeInEvent.hour;
+        const hoursWorked = (timeOutPunch - timeInPunch) / 100;
+        return hoursWorked;
+    } else {
+        const hoursWorked = 0;
+        return hoursWorked;
+    }
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    const date = dateStamp;
+    const hoursWorked = hoursWorkedOnDate.call(this, dateStamp);
+    return hoursWorked * this.payPerHour;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +77,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => totalPayroll + allWagesFor.call(employee), 0);
+}
